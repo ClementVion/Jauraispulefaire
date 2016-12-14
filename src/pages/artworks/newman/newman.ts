3d52ed641baebfbd2fef75d19a7c46dd37de268e
Clément Vion <clement.vion1@gmail.com>
2016-12-14 19:48:34 +0100

    styles change and begin new artwork

--- src/pages/artworks/newman/newman.ts
+++ src/pages/artworks/newman/newman.ts
@@ -1,7 +1,8 @@
-import { Component/*, ViewChild*/ } from '@angular/core';
-// import { Slides } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 // import { SignaturePad } from 'angular2-signaturepad/signature-pad';
+import { AlertController } from 'ionic-angular';
 
 
 @Component({
@@ -11,6 +12,74 @@ import { NavController } from 'ionic-angular';
 
 export class ArtworksPageNewman {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController) {}
+
+  /**
+   * Slider
+   */
+
+  @ViewChild('newmanSlider') slider: Slides;
+
+  mySlideOptions = {
+    direction: "vertical",
+    onlyExternal: true
+  };
+
+  goToNextSlide() {
+    this.slider.slideNext(300);
+  }
+
+  endExperience() {
+    this.navCtrl.pop(ArtworksPageNewman);
+  }
+
+  /**
+   * Go to home dialog
+   */
+
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Etes-vous sûr de vouloir quitter ?',
+      cssClass: 'customAlert',
+      buttons: [
+        {
+          text: 'Oui',
+          cssClass: 'customAlert__buttons customAlert__buttons--yes',
+          handler: () => {
+            this.navCtrl.pop(ArtworksPageNewman);
+          }
+        },
+        {
+          text: 'Non',
+          cssClass: 'customAlert__buttons customAlert__buttons--no'
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  /**
+   * Greetings draw dialog
+   */
+
+  showGreetings() {
+    let alert = this.alertCtrl.create({
+      title: 'Bravo !',
+      cssClass:'greetingsDialog',
+      subTitle: 'Vous avez restauré la toile !',
+      buttons: [
+        {
+          text: 'Suivant',
+          cssClass: 'greetingsDialog__button',
+          handler: () => {
+            this.goToNextSlide();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 
 }
